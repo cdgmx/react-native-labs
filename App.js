32b4d9284a800951d8e0b61ec437adf321f77572
chrismonvitable <iamcdgmx@gmail.com>
2023-04-03 21:22:00 +0800

    Add pushwoosh

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Pushwoosh from 'pushwoosh-react-native-plugin';
+import { DeviceEventEmitter, LogBox, Platform, Alert} from 'react-native';
 
 export default function App() {
+
+  Pushwoosh.init({ 
+      "pw_appid" : "add here"
+  });
+  Pushwoosh.register();
+
+  // this event is fired when the push is received in the app
+  DeviceEventEmitter.addListener('pushReceived', (e) => {
+    console.warn("pushReceived: " + JSON.stringify(e));
+    // shows a push is received. Implement passive reaction to a push, such as UI update or data download.
+  });
+
+  // this event is fired when user clicks on notification
+  DeviceEventEmitter.addListener('pushOpened', (e) => {
+    console.warn("pushOpened: " + JSON.stringify(e));
+    // shows a user tapped the notification. Implement user interaction, such as showing push details
+  });
+    
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
